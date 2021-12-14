6a680a456c5303d98e621278aefd10b8eb9eb6c2
Jozsef123 <jfeher02@gmail.com>
2021-12-13 20:55:19 -0800

    Jozsef's calendar merge

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
 
+import Vue from 'vue'
+import DemoApp from './DemoApp.vue'
+import './main.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTrash)
+library.add(faSignOutAlt)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+require("regenerator-runtime/runtime");
+
 const firebaseConfig = {
   apiKey: "AIzaSyCWY7G0MgW3oG1JYgeRWidIwUdlOEkCiB0",
   authDomain: "calendarapp-2d231.firebaseapp.com",
@@ -14,3 +29,11 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 createApp(App).use(router).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(DemoApp),
+}).$mount(
+  document.body.appendChild(document.createElement('div'))
+)
