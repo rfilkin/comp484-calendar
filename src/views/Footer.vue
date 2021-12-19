b6ac763646b70bb44c4a177e14d7d050464e3317
Jozsef123 <jfeher02@gmail.com>
2021-12-19 00:26:32 -0800

    footer component

---
+++ src/views/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class = "footer">
+    <div class = "content-wrap ">
+        <h2>Calendar Application</h2>        
+        <!-- Social media and contact links. Add or remove any networks. -->
+        <div class = "project-info">
+            <a href="https://docs.google.com/presentation/d/1Ddu3Rup9C0TXqy6rqRcyoJ2ak_CJkgMK/edit?usp=sharing&ouid=116963981487194837043&rtpof=true&sd=true"
+             title = "slides" target="_blank" style="text-decoration:none"><i class="fas fa-file-powerpoint"></i> Project slides&emsp;&emsp;</a> 
+            <a href="https://github.com/rfilkin/comp484-calendar" title = "Github" target="_blank" style="text-decoration:none">
+            <i class="fab fa-github"></i> Github repo&emsp;&emsp;</a>
+            <a href="https://glitch.com/edit/#!/group4-comp484-finalproject" title = "Glitch website" target = "_blank" style="text-decoration:none">
+            <i class="fas fa-fish"></i> Glitch.com</a>
+        </div>
+       <div class = "copyright">
+        <p>Copyright 2021 by Brian Bales, Robert Filkin, Jozsef Feher, 
+            Jericho Orcino, Charles Hernandez
+        </p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+.body{
+    background: #383838;
+}
+.footer{
+  height: 100px;
+  width: 100%;
+  background: #383838;
+  left: 0;
+  bottom: 0;
+  text-align: center;
+  color: white;
+  padding-bottom: 30px;
+}
+
+.project-info{
+    padding-bottom: 10px;
+}
+
+.copyright{
+    padding-bottom: 10px;
+}
+
+h2{
+    padding-top: 10px;
+    padding-bottom: 20px;
+}
+</style>
