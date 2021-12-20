337dcc8202ad30af11ce638e7d97c7315ad15206
Jozsef123 <jfeher02@gmail.com>
2021-12-19 19:57:28 -0800

    firebase

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -7,9 +7,7 @@
             <a href="https://docs.google.com/presentation/d/1Ddu3Rup9C0TXqy6rqRcyoJ2ak_CJkgMK/edit?usp=sharing&ouid=116963981487194837043&rtpof=true&sd=true"
              title = "slides" target="_blank" style="text-decoration:none"><i class="fas fa-file-powerpoint"></i> Project slides&emsp;&emsp;</a> 
             <a href="https://github.com/rfilkin/comp484-calendar" title = "Github" target="_blank" style="text-decoration:none">
-            <i class="fab fa-github"></i> Github repo&emsp;&emsp;</a>
-            <a href="https://glitch.com/edit/#!/group4-comp484-finalproject" title = "Glitch website" target = "_blank" style="text-decoration:none">
-            <i class="fas fa-fish"></i> Glitch.com</a>
+            <i class="fab fa-github"></i> Github repo</a>
         </div>
        <div class = "copyright">
         <p>Copyright 2021 by Brian Bales, Robert Filkin, Jozsef Feher, 
@@ -30,14 +28,14 @@
     background: #383838;
 }
 .footer{
-  height: 120px;
+  height: 130px;
   width: 100%;
   background: #383838;
   left: 0;
   bottom: 0;
   text-align: center;
   color: white;
-  padding-bottom: 30px;
+  margin-bottom: 90px;
 }
 
 .project-info{
@@ -45,7 +43,7 @@
 }
 
 .copyright{
-    padding-bottom: 10px;
+    margin-bottom: 20px;
 }
 
 h2{
